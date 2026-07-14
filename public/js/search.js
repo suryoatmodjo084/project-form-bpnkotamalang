d7c412d62dbb03fd96b732f72525ccd001fbcf58
suryoatmodjo084 <82429035+suryoatmodjo084@users.noreply.github.com>
2026-07-14 17:41:27 +0700

    Add files via upload

---
+++ public/js/search.js
@@ -0,0 +1,56 @@
+jQuery(function() {
+  // Initalize lunr with the fields it will be searching on. I've given title
+  // a boost of 10 to indicate matches on this field are more important.
+  window.idx = lunr(function () {
+    this.field('title', { boost: 10 });
+    this.field('category');
+    this.ref('id');
+  });
+
+  // Download the data from the JSON file we generated
+  window.data = $.getJSON('/search_data.json');
+
+  // Wait for the data to load and add it to lunr
+  window.data.done(function(loaded_data){
+    $.each(loaded_data, function(index, value){
+      window.idx.add(
+        $.extend({ "id": index }, value)
+      );
+    });
+  });
+
+  // Event when the form is submitted
+  $("#site_search").submit(function(){
+      event.preventDefault();
+      var query = $("#search_box").val(); // Get the value for the text field
+      var results = window.idx.search(query); // Get lunr to perform a search
+      display_search_results(results); // Hand the results off to be displayed
+  });
+
+  function display_search_results(results) {
+    var $search_results = $("#tbl-index");
+
+    // Wait for data to load
+    window.data.then(function(loaded_data) {
+
+      // Are there any results?
+      if (results.length) {
+        $search_results.empty(); // Clear any old results
+
+        // Iterate over the results
+        results.forEach(function(result) {
+          var item = loaded_data[result.ref];
+
+          // Build a snippet of HTML for this result
+          console.log(item);
+          var appendString = '<tr><td><a href="' + item.url.trim() + '">' + item.title + '</a></td><td>'+ item.date +'</td></tr>';
+
+          // Add it to the results
+          $search_results.append(appendString);
+        });
+      } else {
+        $search_results.html('<tr><td span="2">Ooops! No Result Found</td></tr>');
+      }
+    });
+  }
+});
